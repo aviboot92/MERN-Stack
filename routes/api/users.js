ec523a29ec3c38aae4fab161cfed15a2d47a3da8
avkarra <avkarra@ncompasbusiness.com>
2020-06-13 11:43:57 +0530

    Basic Validation for Register Usre route is generated with server side validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const router =  express.Router();
+const router = express.Router();
+const {check, validationResult} = require('express-validator');
 
-//  @route GET api/users
-// @desc Test router
-// @access PUBLIC
-router.get('/', (req,res) => res.send('User route'));
+//  @route Post api/users @desc Test router @access PUBLIC
+router.post('/', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include a valid eamil').isEmail(),
+    check('password', 'Password is required and must be atleast 8 characters long').isLength({min: 8})
+], (req, res) => {
+    console.log(req.body.name);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res
+            .status(400)
+            .json({
+                errors: errors.array()
+            });
+    }
+    res.send('User route');
+});
 
 module.exports = router;
