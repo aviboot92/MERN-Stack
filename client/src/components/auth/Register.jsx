528f6115eae390781e8d095d75905a6d9004fa6d
avkarra <avkarra@ncompasbusiness.com>
2020-06-15 16:37:27 +0530

    Register Form  and useHook

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, {Fragment, useState} from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Register() {
+
+    const [formData,
+        setFormData] = useState({name: '', email: '', password: '', password2: ''});
+
+    const {name, email, password, password2} = formData;
+
+    const onChangeHandler = e => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(password !== password2){
+            console.log('Password mismatch')
+        } else {
+            console.log(formData);
+        }
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>
+                Create Your Account</p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        onChange={onChangeHandler}
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        required/>
+                </div>
+                <div className="form-group">
+                    <input
+                        onChange={onChangeHandler}
+                        type="email"
+                        placeholder="Email Address"
+                        value={email}
+                        name="email"
+                        required/>
+                    <small className="form-text">This site uses Gravatar so if you want a profile image, Please create an account in the Gravatar with same email</small>
+                </div>
+                <div className="form-group">
+                    <input
+                        onChange={onChangeHandler}
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        name="password"
+                        required
+                        minLength="8"/>
+                </div>
+                <div className="form-group">
+                    <input
+                        onChange={onChangeHandler}
+                        type="password"
+                        placeholder="Confirm Password"
+                        value={password2}
+                        name="password2"
+                        required
+                        minLength="8"/>
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+            <p className="my-1">
+                Already have an account?
+                <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
     )
 }
