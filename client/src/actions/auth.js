8442b47a39b720b23f1e7a6d9136cb64723be513
avkarra <avkarra@ncompasbusiness.com>
2020-06-16 18:06:25 +0530

    Load User and set Auth Token

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,8 +1,30 @@
 import axios from 'axios';
-import {SET_ALERT, setAlert} from './alert';
-import {REGISTER_SUCCESS, REGISTER_FAIL} from './types';
+import { setAlert} from './alert';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types';
+import setAuthToken from './../utils/setAuthToken';
 
-// Register User with
+// Load User
+export const loadUser = () => async dispatch => {
+    if(localStorage.token){
+        setAuthToken(localStorage.token);
+    }
+
+    try {
+        const res = await axios.get('/api/auth');
+        dispatch({
+            
+            type: USER_LOADED,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            
+            type: AUTH_ERROR
+        })
+    }
+}
+
+// Register User 
 export const register = ({name, email, password}) => async dispatch =>{
     const config = {
         headers: {
@@ -28,4 +50,7 @@ export const register = ({name, email, password}) => async dispatch =>{
         })
     }
 }
+
+
+
 
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import {REGISTER_SUCCESS, REGISTER_FAIL} from './../actions/types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -7,28 +7,40 @@ const initialState = {
     user: null
 }
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
-        case REGISTER_SUCCESS : {
-            localStorage.setItem('token', action.payload.token);
+        case USER_LOADED:
             return {
                 ...state,
-                ...action.payload,
                 isAuthenticated: true,
-                loading: false
+                loading: false,
+                user: action.payload
             }
-        }
 
-        case REGISTER_FAIL: {
-            localStorage.removeItem('token');
-            return {
-                ...state,
-                token: null,
-                isAuthenticated: false,
-                loading: false
+        case REGISTER_SUCCESS:
+            {
+                localStorage.setItem('token', action.payload.token);
+                return {
+                    ...state,
+                    ...action.payload,
+                    isAuthenticated: true,
+                    loading: false
+                }
             }
-        }
 
-        default : return state;
+        case REGISTER_FAIL:
+        case AUTH_ERROR:
+            {
+                localStorage.removeItem('token');
+                return {
+                    ...state,
+                    token: null,
+                    isAuthenticated: false,
+                    loading: false
+                }
+            }
+
+        default:
+            return state;
     }
 }
