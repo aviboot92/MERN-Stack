c070dd9350723f9cde60db77e4f149cc54c8a598
avkarra <avkarra@ncompasbusiness.com>
2020-08-13 17:16:57 +0530

    create profile component

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ import Dashboard from './components/dashboard/Dashboard';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
 import {loadUser} from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -35,6 +36,7 @@ const App = () => {
                             <Route exact path='/login' component={Login}/>
                             <Route exact path='/register' component={Register}/>
                             <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                            <PrivateRoute exact path='/create-profile' component={CreateProfile}/>
                         </Switch>
                     </section>
                 </Fragment>
